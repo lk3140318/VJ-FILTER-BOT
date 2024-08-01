74626c5499324ff41e498e6e97df9b92c208b5a6
NiTiN94U <nitin992516@gmail.com>
2024-08-01 12:19:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,8 +107,8 @@ VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/Howtodownload945/
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'mypowerlinks.org')
-SHORTLINK_API = environ.get('SHORTLINK_API', '767dfd3dd5978c4ef51987239546ff594853a40e')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
+SHORTLINK_API = environ.get('SHORTLINK_API', '')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
