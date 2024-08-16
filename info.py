4cd2f103c5a4bd53be36ec7a96a94978ade3f979
NiTiN94U <nitin992516@gmail.com>
2024-08-16 11:41:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesgroup94')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+hgF1HH9P4pMxZjA1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movies9486')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Howtodownload945/3')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'nitin947') # Support Chat Link Without https:// or @
@@ -106,7 +106,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/Howtodownload945/3')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
 SHORTLINK_API = environ.get('SHORTLINK_API', '')
 
